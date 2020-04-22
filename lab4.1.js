4a23ba5a342b462482c8bf709e83e147059e175f
Julia <madam.julia.26@yandex.ru>
2020-04-22 11:01:46 +0300

    first commit

---
+++ lab4.1.js
@@ -0,0 +1,5 @@
+export default function addName() {
+let elem = document.getElementById("app")
+elem.innerHTML = "Симонова Юлия Михайловна M3306";
+}
+
---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+var path = require('path');
+
+module.exports = {
+  entry: './index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve('C:\Users\User1\Documents\ИТМО третий курс\веб\Лаб4', 'dist')
+  }
+};
